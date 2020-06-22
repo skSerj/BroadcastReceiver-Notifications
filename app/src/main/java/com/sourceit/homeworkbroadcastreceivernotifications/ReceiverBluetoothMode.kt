4382ae2725d781fcd35c4b1fcaccbe942e520ce2
kuzavkov <serg1992kuzavkov@gmail.com>
2020-06-22 23:24:54 +0300

    fix bug with Bluetooth Receiver

--- app/src/main/java/com/sourceit/homeworkbroadcastreceivernotifications/ReceiverBluetoothMode.kt
+++ app/src/main/java/com/sourceit/homeworkbroadcastreceivernotifications/ReceiverBluetoothMode.kt
@@ -6,22 +6,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.sourceit.appnotification.arsenal.chooseNotification
 import com.sourceit.appnotification.arsenal.chooseNotificationForBluetooth
+import com.sourceit.homeworkbroadcastreceivernotifications.utilites.isAirplaneModeOn
+
 class ReceiverBluetoothMode : BroadcastReceiver() {
 
 
     companion object {
-        fun newInstance() = ReceiverAirplaneMode()
+        fun newInstance() = ReceiverBluetoothMode()
     }
 
     override fun onReceive(context: Context, intent: Intent?) {
         val bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         val nm = context.getSystemService(Context.NOTIFICATION_SERVICE)
                 as NotificationManager
-        if (!bluetoothAdapter.isEnabled) {
+        if (!bluetoothAdapter.isEnabled && isAirplaneModeOn(context)) {
             nm.cancel(101)
+        } else if (bluetoothAdapter.isEnabled && isAirplaneModeOn(context)) {
+            Log.d("MyApp", "bluetooth is ON & airplaneMode is ON")
+            chooseNotification(context)
         } else {
-            Log.d("MyApp", "bluetooth is ON")
+            Log.d("MyApp", "bluetooth is ON & airplaneMode is OFF")
             chooseNotificationForBluetooth(context)
         }
     }
